fb9dde8ecb5b0030d0e16e64a4d8f48b65015589
houssem eddine <75279465+Tweeel@users.noreply.github.com>
2021-07-07 13:53:00 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.HelloConstraint;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Log.d("MainActivity", "Hello World");
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null) mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void ResetToZero(View view) {
+        mCount=0;
+        if(mShowCount!=null) mShowCount.setText(Integer.toString(mCount));
+    }
+}
